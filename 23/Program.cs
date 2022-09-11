92702509a22c14cfe6dbe6c648936dd85ba8f813
Юлия <>
2022-09-11 21:34:29 +0300

    23

---
+++ 23/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int kub = 1;
+
+while (i<=N)
+{
+    kub = i * i * i;
+    i++;
+    Console.WriteLine($"Куб {kub}");
+}
